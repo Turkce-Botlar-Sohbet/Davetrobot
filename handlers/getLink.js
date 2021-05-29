a1355ac5284e44f1091521311fb6084ef09b06f3
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2021-05-29 18:51:29 +0300

    Update getLink.js

--- handlers/getLink.js
+++ handlers/getLink.js
@@ -1,22 +1,29 @@
+const Chat = process.env.CHAT_ID.split(';');
+
 module.exports = async (ctx) => {
     if (ctx.chat.type !== 'private') return null;
     try {
-        const user = await ctx.telegram.getChatMember(process.env.CHAT_ID, ctx.chat.id)
+        const user = await ctx.telegram.getChatMember(Chat[0], ctx.chat.id)
         if (user.status === 'kicked') {
             await ctx.replyWithHTML(process.env.BAN_MESSAGE)
         } else {
-            const chatLink = await ctx.telegram.exportChatInviteLink(process.env.CHAT_ID);
-            await ctx.replyWithHTML(process.env.START_MESSAGE, {
-                reply_to_message_id: ctx.message.message_id, reply_markup: {
-                    inline_keyboard: [[
-                        {
-                            text: process.env.BUTTON_MESSAGE,
-                            url: `${chatLink}`
-                        }
-                    ]]
-                }
-            });
-        }
+            for (let i in Chat) {
+                await ctx.telegram.getChat(Chat[i]).then(async function (result) {
+                const chatLink = await ctx.telegram.exportChatInviteLink(Chat[i]);
+                await ctx.replyWithHTML(process.env.START_MESSAGE, {
+                     reply_markup: {
+                        inline_keyboard: [[
+                            {
+                                text: `${result.title}`,
+                                url: `${chatLink}`
+                            }
+                        ]]
+                      }
+                   })
+                })
+              }
+
+         }
 
     } catch (error) {
         console.log(`Error: ${error}`);
