51125573ff2ae91340eac676ce72f85c1e7fe586
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2021-05-30 02:26:59 +0300

    Fixed

--- handlers/getLink.js
+++ handlers/getLink.js
@@ -1,4 +1,6 @@
 const Chat = process.env.CHAT_ID.split(';');
+const { Markup } = require('telegraf')
+
 
 module.exports = async (ctx) => {
     if (ctx.chat.type !== 'private') return null;
@@ -7,26 +9,17 @@ module.exports = async (ctx) => {
         if (user.status === 'kicked') {
             await ctx.replyWithHTML(process.env.BAN_MESSAGE)
         } else {
-            for (let i in Chat) {
-                await ctx.telegram.getChat(Chat[i]).then(async function (result) {
-                const chatLink = await ctx.telegram.exportChatInviteLink(Chat[i]);
-                await ctx.replyWithHTML(process.env.START_MESSAGE, {
-                     reply_markup: {
-                        inline_keyboard: [[
-                            {
-                                text: `${result.title}`,
-                                url: `${chatLink}`
-                            }
-                        ]]
-                      }
-                   })
-                })
-              }
-
+            
+            let chatLink = await Promise.all(Chat.map(async (a, i) => {
+                return ctx.telegram.exportChatInviteLink(`${a}`)
+            }))
+            i = 0
+            await ctx.replyWithHTML(
+                process.env.START_MESSAGE,
+                Markup.inlineKeyboard(`${chatLink}`.match(/(?:(?:https?):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+/g).map(x => [Markup.button.url('k/g ' + ++i, x)])))
          }
 
     } catch (error) {
         console.log(`Error: ${error}`);
     }
 };
-
