9e82738f6d620bbd724af295ee6af228e1f48980
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2021-06-20 14:50:34 +0300

    Create antiflood.js

---
+++ utils/antiflood.js
@@ -0,0 +1,18 @@
+function AntiFlood() {
+    this.users = {}
+
+    this.isFlooding = (userId) => {
+        this.users[userId] = (this.users[userId] || 0) + 1
+        return (this.users[userId] >= 2)
+    }
+
+    setInterval(() => {
+        for (var i in this.users) {
+            if (this.users[i] <= 0) delete this.users[i]
+            else if (this.users[i] > 10) this.users[i] = 10
+            else this.users[i]--
+        }
+    }, 6000)
+}
+
+module.exports = AntiFlood
